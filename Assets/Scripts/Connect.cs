8170095bc4e2dee3c71add561c8fdf62b6bf5134
Alexey <segs509@gmail.com>
2020-09-22 20:38:50 +0300

    Photon connection

---
+++ Assets/Scripts/Connect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Connect : MonoBehaviourPunCallbacks
+{
+    public GameObject Player;
+    public GameObject SpawnPoint;
+
+    public void StartGame()
+    {
+        PhotonNetwork.GameVersion = "0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom("", new Photon.Realtime.RoomOptions {MaxPlayers = 20});
+    }
+
+    public override void OnJoinedRoom()
+    {
+        GameObject Human = PhotonNetwork.Instantiate(Player.name, SpawnPoint.transform.position, Quaternion.identity);
+        Human.GetComponent<FirstPersonController>().enabled = true;
+        
+    }
+
+    public void Start()
+    {
+        StartGame();
+    }
+}
