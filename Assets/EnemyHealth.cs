0f3ce6160b8d95ead0b0bd4b1deaea20f51ed425
Alexey <segs509@gmail.com>
2020-09-22 18:34:13 +0300

    Shoot/EnemyHealth

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Cache;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float HP = 10;
+
+    public void AddDamage(float damage)
+    {
+        HP -= damage;
+        Debug.Log("Text: " + HP);
+        if (HP <= 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float damage = 1;
+    public float timeout = 0.2f;
+    public string[] targetTags = { "Target_1"};
+    private float curTimeout;
+
+	void Update()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(transform.position);
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (Input.GetMouseButton(0))
+			{				
+				foreach (string currentTag in targetTags)
+				{
+					if (currentTag == hit.transform.tag)
+					{
+						curTimeout += Time.deltaTime;
+						if (curTimeout > timeout)
+						{
+							Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward * 100f, Color.red, duration: 2f);
+							curTimeout = 0;
+							hit.transform.GetComponent<EnemyHealth>().AddDamage(damage);
+							Debug.Log("damage");
+						}
+					}
+				}
+			}
+			else
+			{
+				curTimeout = timeout + 1;
+			}
+		}
+	}
+}
